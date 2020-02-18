a5fe39adefbdd1a52281b82cd7d35a90109838cd
Zhe Xuan Chua <bluefish6543@gmail.com>
2020-02-18 09:43:07 +0000

    Add digital receiver test

---
+++ arduino-tests/DigitalReadSerial/DigitalReadSerial.ino
@@ -0,0 +1,18 @@
+int inputPin = 11;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // make the pushbutton's pin an input:
+  pinMode(inputPin, INPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input pin:
+  int value = digitalRead(inputPin);
+  // print out the state of the button:
+  Serial.println(value);
+  delay(200);
+}
