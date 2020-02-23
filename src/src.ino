9211565a52b3df9a9061c750dd7b8eeb9e2c8320
Zhe Xuan Chua <bluefish6543@gmail.com>
2020-02-23 22:15:33 +0000

    Add loop() code and fix errors with static consts

--- src/src.ino
+++ src/src.ino
@@ -269,9 +269,9 @@ class Robot {
     void locateRobot() {
       /* Locates target robot and turns towards it. */
       servoAngle = 0; // reset every time we call it
-      int signalDirection == scanIR();
+      int signalDirection = scanIR();
       
-      while (signalDirection == NO_IR_SIGNAL_FOUND || (!requiresServicing() && !allServicingRobotsCollected) {
+      while (signalDirection == NO_IR_SIGNAL_FOUND || (!requiresServicing() && !allServicingRobotsCollected)) {
         moveRobotToNextLocation();
         signalDirection = scanIR();
       }
@@ -304,7 +304,31 @@ class Robot {
      *  Predetermined route for robot
      *  OpenCV
      */
+     
+  public:
+    void start() {
+      state = START_TO_TUNNEL;
+      followLine(); // this should take the robot all the way to the end of the tunnel
+      moveForward(3); // constant to be changed, this moves the robot completely out of the tunnel
+      state = SEARCH;
+      findRobot();
+      // to be continued
+    }
 };
+
+const int Robot::MAX_SERVO_ANGLE;
+const int Robot::DISTANCE_THRESHOLD;
+const int Robot::NO_IR_SIGNAL_FOUND;
+const int Robot::MOVE_FORWARD_CALIBRATION_CONSTANT;
+const int Robot::TURN_CALIBRATION_CONSTANT;
+const int Robot::NORMAL_MOTOR_SPEED;
+const int Robot::TURN_DELAY;
+const int Robot::IR_SCAN_DELAY;
+const int Robot::ADAPTIVE_THRESHOLD_OFFSET;
+const int Robot::SERVO_ROTATION_STEP;
+const int Robot::IR_SCAN_WINDOW_LENGTH;
+const int Robot::IR_PEAK_THRESHOLD;
+const int Robot::DEFAULT_MOVE_FORWARD_DURATION;
 
 void setup() {
   AFMS.begin();
@@ -313,5 +337,6 @@ void setup() {
 }
 
 void loop() {
-  
+  Robot(robot);
+  robot.start();
 }
