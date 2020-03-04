a102d50370d06da198c9626aeb3ccfc5fb93e30c
Zhe Xuan Chua <bluefish6543@gmail.com>
2020-03-04 10:15:06 +0000

    Remove unnecessary function

--- src/imaging.py
+++ src/imaging.py
@@ -17,25 +17,6 @@ class Image:
         self.num_colours = num_colours
         self.kernel_size = kernel_size
 
-    def _draw_axis(self, p, q, colour, scale=0.2):
-        angle = np.arctan2(p[1] - q[1], p[0] - q[0])
-        hypotenuse = np.sqrt((p[1] - q[1]) ** 2 + (p[0] - q[0]) ** 2)
-        q = np.zeros_like(q, dtype=np.int64)
-
-        # Lengthen the arrow by a factor of scale
-        q[0] = int(p[0] - scale * hypotenuse * np.cos(angle))
-        q[1] = int(p[1] - scale * hypotenuse * np.sin(angle))
-        cv2.line(self.img, (p[0], p[1]), (q[0], q[1]), colour, 1, cv2.LINE_AA)
-
-        # Create the arrow hooks
-        p[0] = int(q[0] + 9 * np.cos(angle + np.pi / 4))
-        p[1] = int(q[1] + 9 * np.sin(angle + np.pi / 4))
-        cv2.line(self.img, (p[0], p[1]), (q[0], q[1]), colour, 1, cv2.LINE_AA)
-
-        p[0] = int(q[0] + 9 * np.cos(angle - np.pi / 4))
-        p[1] = int(q[1] + 9 * np.sin(angle - np.pi / 4))
-        cv2.line(self.img, (p[0], p[1]), (q[0], q[1]), colour, 1, cv2.LINE_AA)
-
     def _get_orientation(self, pts):
         # Construct a buffer used by the PCA analysis
         data_pts = np.squeeze(np.array(pts, dtype=np.float64))
@@ -56,8 +37,6 @@ class Image:
         cv2.circle(self.img, (cntr[0], cntr[1]), 3, (255, 0, 255), 1)
         p1 = cntr + 0.02 * eigenvectors[0] * eigenvalues[0]
         p2 = cntr - 0.02 * eigenvectors[1] * eigenvalues[1]
-        # self._draw_axis(copy.copy(cntr), p1, (0, 255, 0), 2)
-        # self._draw_axis(copy.copy(cntr), p2, (255, 255, 0), 10)
 
         return np.arctan2(eigenvectors[0, 1], eigenvectors[0, 0])  # orientation in radians
 
@@ -162,7 +141,7 @@ def take_photo():
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     receive_data(sock)
-    # take_photo()
+    take_photo()
     src = cv2.imread(os.path.join(os.getcwd(), 'images', 'test.jpg'))
     assert src is not None
     image = Image(copy.copy(src), threshold=2, num_colours=8, kernel_size=5)
